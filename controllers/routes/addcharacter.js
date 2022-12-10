569d9ae5b5de8e106c051a5a320cb4d5a556cbf5
Marco Aurélio Ghelere <cau.ghelere@hotmail.com>
2022-12-10 14:45:43 -0300

    added img upload

--- controllers/routes/addcharacter.js
+++ controllers/routes/addcharacter.js
@@ -2,6 +2,8 @@ const express = require('express');
 const character = require('../../models/characters');
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const fetch = require("node-fetch");
 
 function tokenCheck(req, res, next) {
     const token = req.headers.token;
@@ -19,13 +21,34 @@ function tokenCheck(req, res, next) {
     });
 }
 
-router.post("/", tokenCheck, (req, res) => {
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-        const addCharacter = new character({name: req.body.name,});
+router.post("/", tokenCheck, upload.single("charPic"), (req, res) => {
+
+    fetch(`${process.env.IMG_LINK}`, {
+        method: "POST",
+        headers: { "Content-Type": "image/png" },
+        body: req.file.buffer,
+    })
+
+    .then((response) => response.json())
+    .then((data) => {
+        const addCharacter = new character({name: req.body.name, link: data.url,});
 
         addCharacter.save();
 
-        return res.status(200).json({ msg: "Character created" });
+        return res.status(200).json({ msg: "Character created" });   
+    },
+    (error) => {
+        console.error('Error:', error);
+    }
+    );
+
+    
+    
+
+        
 });
 
 module.exports = router;
--- models/characters.js
+++ models/characters.js
@@ -8,7 +8,9 @@ const Character = new Schema({
     name: { 
         type: String, required: true
      },
-
+     link: {
+        type: String, required: true
+     },
 });
 
 module.exports = mongoose.model("Character", Character);
